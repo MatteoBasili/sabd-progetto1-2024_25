7baf13900baef2e79b2834fb5d3cba0ab09b7f35
MatteoBasili <matthew2000.mb@gmail.com>
2025-05-24 10:50:13 +0200

    Il namenode non va più in SAFE mode al riavvio dei container

--- hdfs/init-datanode.sh
+++ hdfs/init-datanode.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-rm -rf /opt/hadoop/data/dataNode/*
-chown -R hadoop:hadoop /opt/hadoop/data/dataNode
-chmod 755 /opt/hadoop/data/dataNode
+
+# Inizializza solo se la directory è vuota
+if [ ! -f /opt/hadoop/data/dataNode/current/VERSION ]; then
+  echo "Prima inizializzazione del DataNode: formatto la directory"
+  rm -rf /opt/hadoop/data/dataNode/*
+  chown -R hadoop:hadoop /opt/hadoop/data/dataNode
+  chmod 755 /opt/hadoop/data/dataNode
+else
+  echo "DataNode già inizializzato, non cancello nulla"
+fi
+
 hdfs datanode
+
