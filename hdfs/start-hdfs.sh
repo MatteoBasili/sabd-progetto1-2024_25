f5e42c69930c57319fdd4d34e73e0f01b5510d65
MatteoBasili <matthew2000.mb@gmail.com>
2025-05-27 14:30:23 +0200

    Update

--- hdfs/start-hdfs.sh
+++ hdfs/start-hdfs.sh
@@ -4,18 +4,7 @@
 if [ ! -d "/opt/hadoop/data/nameNode/current" ]; then
     echo "Formatting NameNode..."
     hdfs namenode -format
-    
-    # Avvia temporaneamente il NameNode in background per poter creare le cartelle
-    hdfs --daemon start namenode
-    sleep 10 #5  # Attendi un po' per avvio
-    
-    echo "Creating HDFS directory /output..."
-    hdfs dfs -mkdir -p /output
-    hdfs dfs -chmod -R 777 /output
-
-    # Termina il NameNode temporaneo
-    hdfs --daemon stop namenode
 fi
 
-# Avvia normalmente il NameNode in foreground
+# Avvia normalmente il NameNode
 hdfs namenode
